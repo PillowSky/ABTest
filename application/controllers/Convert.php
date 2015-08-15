67ab0e44ff608e53b8dd2be50b7158e7b72a9b67
Yuzo <hyzgog@gmail.com>
2015-08-15 14:01:11 +0800

    add Convert.php

---
+++ application/controllers/Convert.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Convert extends CI_Controller {
+	public function index() {
+		if (is_cli()) {
+			$directory = dir("/var/www/simple/");
+
+			while (($filename = $directory->read()) !== FALSE) {
+				$ID = substr($filename, 0, strrpos($filename, '_'));
+				if ($ID) {
+					$this->png2jpg("/var/www/simple/${ID}_simple.png", "/var/www/simple/${ID}_simple.jpg", 100);
+					echo($ID . "\n");
+				}
+			}
+			$directory->close();
+		}
+	}
+
+	private function png2jpg($src, $dest, $quality) {
+	    $img = imagecreatefrompng($src);
+	    imagejpeg($img, $dest, $quality);
+	    imagedestroy($img);
+	}
+}
